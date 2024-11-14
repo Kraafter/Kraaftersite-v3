e46e52bb136c22220f96dcfa66e2248753885939
Kraafter <110327339+Kraafter@users.noreply.github.com>
2024-11-14 15:05:54 +0700

    added links page 2

--- src/routes/link/+page.svelte
+++ src/routes/link/+page.svelte
@@ -86,19 +86,129 @@
         height: 100%;
         padding: 30px;
         padding-top: 5px;
+        margin: 80px;
+        margin-top: 5px;
+        background: linear-gradient(90deg, rgba(50,50,50,0) 0%, rgba(50,50,50,0.31) 1%, rgba(50,50,50,0.45) 2%, rgba(50,50,50,0.54) 4%, rgba(50,50,50,0.59) 5%, rgba(50,50,50,0.6) 7%, rgba(50,50,50,0.6) 93%, rgba(50,50,50,0.59) 95%, rgba(50,50,50,0.54) 96%, rgba(50,50,50,0.45) 98%, rgba(50,50,50,0.31) 99%, rgba(50,50,50,0) 100%);
     }
 
     .main{
-        margin: 80px;
-        margin-top: 5px;
-        background-color: rgba(80, 80, 80, 0.6);
         display:flex;
         flex-direction: column;
-        padding: 20px;
+    }
+
+    .main2{
+        display:flex;
+        flex-direction: row;
+        width: 50%;
+    }
+
+    .main4{
+        display:flex;
+        flex-direction: row;
+        width: 100%;
+    }
+
+    .main3{
+        display:flex;
+        flex-direction: row;
+    }
+
+    .linkitem {
+        width: 33%;
+        height: 35px;
+        margin: 5px;
+        border-radius: 3px;
+        background-color: rgb(50,50,50);
+        border-width: thin;
+        border-style: outset;
+        border-color: aliceblue;
+    }
+
+    .linkitem2 {
+        width: 50%;
+        height: 35px;
+        margin: 5px;
+        border-radius: 3px;
+        background-color: rgb(50,50,50);
+        border-width: thin;
+        border-style: outset;
+        border-color: aliceblue;
     }
 
     .text1 {
         font-family: monospace;
         color: aliceblue;
+        font-size: 30px;
+        padding: 10px;
+    }
+
+    .text2 {
+        font-family: monospace;
+        color: aliceblue;
+        font-size: 20px;
+        padding: 10px;
+        margin-bottom: 0;
+    }
+
+    .text3 {
+        font-family: monospace;
+        color: aliceblue;
+        font-size: 20px;
+        padding: auto;
+        text-align: center;
+        width: fit-content;
+        margin: auto;
+        margin-top: 3px;
+    }
+
+    @media (max-width: 768px) {
+
+    }
+
+    /* Medium devices (tablets, less than 992px) */
+    @media (max-width: 991.98px) {
+        .main2 {
+            margin: 0px 5px;
+            margin-top: 10px;
+            flex-direction: column;
+        }
+
+        .main4 {
+            margin: 0px 5px;
+            margin-top: 10px;
+            flex-direction: column;
+        }
+
+        .full {
+            padding: 10px;
+            margin: 5px;
+        }
+
+        .text1 {
+            font-size: 25px;
+            padding-bottom: 0px;
+        }
+
+        .text2 {
+            font-size: 15px;
+            padding-top: 0px;
+        }
+
+        .text3 {
+            font-size: 15px;
+            margin-top: 6px;
+        }
+
+        .linkitem {
+            width: calc(100% - 10px);
+        }
+
+        .linkitem2 {
+            width: calc(100% - 20px);
+        }
+
+        .top {
+            margin: 0px 5px;
+        }
     }
 </style>
