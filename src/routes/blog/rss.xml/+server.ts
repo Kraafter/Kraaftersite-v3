540196809423908b79403f6da1c10f7c7cd6d752
Kraafter <110327339+Kraafter@users.noreply.github.com>
2024-11-20 20:52:32 +0700

    rss formatting

--- src/routes/blog/rss.xml/+server.ts
+++ src/routes/blog/rss.xml/+server.ts
@@ -9,26 +9,26 @@ export async function GET({ fetch }) {
 
 	const xml = `
 		<rss xmlns:atom="http://www.w3.org/2005/Atom" version="2.0">
-			<channel>
-				<title>${config.title}</title>
-				<description>${config.description}</description>
-				<link>${config.url}</link>
-				<atom:link href="${config.url}blog/rss.xml" rel="self" type="application/rss+xml"/>
-				${posts
-					.map(
-						(post) => `
-						<item>
-							<title>${post.title}</title>
-							<description>${post.description}</description>
-							<link>${config.url}blog/posts/${post.slug}</link>
-							<guid isPermaLink="true">${config.url}blog/posts/${post.slug}</guid>
-							<pubDate>${new Date(post.date).toUTCString()}</pubDate>
-						</item>
-					`
-					)
-					.join('')}
-			</channel>
-		</rss>
+	<channel>
+		<title>${config.title}</title>
+		<description>${config.description}</description>
+		<link>${config.url}</link>
+		<atom:link href="${config.url}blog/rss.xml" rel="self" type="application/rss+xml"/>
+		${posts
+			.map(
+				(post) => `
+				<item>
+					<title>${post.title}</title>
+					<description>${post.description}</description>
+					<link>${config.url}blog/posts/${post.slug}</link>
+					<guid isPermaLink="true">${config.url}blog/posts/${post.slug}</guid>
+					<pubDate>${new Date(post.date).toUTCString()}</pubDate>
+				</item>
+			`
+			)
+			.join('')}
+	</channel>
+</rss>
 	`.trim()
 
 	return new Response(xml, { headers })
