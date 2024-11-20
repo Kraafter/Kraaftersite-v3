9d6dfa8427c809a8e86327b2b097d1c6b6753797
Kraafter <110327339+Kraafter@users.noreply.github.com>
2024-11-20 19:36:56 +0700

    removed database functionality

--- src/routes/api/gallery/photo/+server.ts
+++ src/routes/api/gallery/photo/+server.ts
@@ -1,5 +1,4 @@
 import { json } from "@sveltejs/kit";
-import db from '$lib/database';
 
 const url = 'https://api.imagekit.io/v1/files?path=photo';
 const options = {
@@ -9,30 +8,12 @@ const options = {
     Authorization: import.meta.env.VITE_APIKEY // DO NOT PUSH PLAIN API KEY, USE SECRETS
   }
 };
-const CACHE_DURATION = 86400000; // 1 day in milliseconds
 
 export async function GET() {
-    const cachedData = await db.cachedPhoto.findFirst({
-        orderBy: {createdAt: 'desc'}
-    })
-
-    const now = new Date();
-
-    if (cachedData && (now.getTime() - new Date(cachedData.createdAt).getTime()) < CACHE_DURATION) {
-        console.log("returning cached data")
-        const resjs = await JSON.parse(cachedData.data)
-        return json(resjs)
-    }
 
     console.log("fetching data from api")
     const res = await fetch(url, options);
     const resjs = await res.json();
-    
-    await db.cachedPhoto.create({
-        data: {
-            data: JSON.stringify(resjs),
-        }
-    })
 
     return json(resjs);
 }
