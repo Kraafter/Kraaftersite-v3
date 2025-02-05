0e228297a693ca41e2c49e8fb883bc1c9b15351f
Kraafter <110327339+Kraafter@users.noreply.github.com>
2025-02-05 23:29:25 +0700

    Added linking contrast for privacy policy page

--- src/routes/privacy/+page.svelte
+++ src/routes/privacy/+page.svelte
@@ -25,17 +25,22 @@
     <p class="text2">
         This website also uses some external service providers. Their names, usage, privacy policy, and metrics I can view, are listed below:
         <ul class="text2" style="margin-left: 20px;">
-            <li>Vercel:<a class="text2" href="https://vercel.com/legal/privacy-policy">Hosting (User agent, accessed links/functions)</a></li>
-            <li>Namecheap:<a class="text2" href="https://www.namecheap.com/legal/general/privacy-policy/">Domain registrar</a></li>
-            <li>Cloudflare:<a class="text2" href="https://www.cloudflare.com/privacypolicy/">DNS management (Region trend, resolve percentage)</a></li>
-            <li>Utterances:<a class="text2" href="https://github.com/productinfo/utterances/blob/master/PRIVACY-POLICY.md">Blog commenting (GitHub cookie)</a></li>
-            <li>Johnvertisements:<a class="text2" href="https://john.citrons.xyz/">Johnvertisements</a></li>
-            <li>ImageKit:<a class="text2" href="https://imagekit.io/privacy-policy-new/">Image hosting (Accessed images)</a></li>
-            <li>Github:<a class="text2" href="https://docs.github.com/en/site-policy/privacy-policie">Listing project details</a></li>
+            <li>Vercel:<a class="text2 linking" href="https://vercel.com/legal/privacy-policy">Hosting (User agent, accessed links/functions)</a></li>
+            <li>Namecheap:<a class="text2 linking" href="https://www.namecheap.com/legal/general/privacy-policy/">Domain registrar</a></li>
+            <li>Cloudflare:<a class="text2 linking" href="https://www.cloudflare.com/privacypolicy/">DNS management (Region trend, resolve percentage)</a></li>
+            <li>Utterances:<a class="text2 linking" href="https://github.com/productinfo/utterances/blob/master/PRIVACY-POLICY.md">Blog commenting (GitHub cookie)</a></li>
+            <li>Johnvertisements:<a class="text2 linking" href="https://john.citrons.xyz/">Johnvertisements</a></li>
+            <li>ImageKit:<a class="text2 linking" href="https://imagekit.io/privacy-policy-new/">Image hosting (Accessed images)</a></li>
+            <li>Github:<a class="text2 linking" href="https://docs.github.com/en/site-policy/privacy-policie">Listing project details</a></li>
         </ul>
 </main>
 
 <style>
+
+    .linking {
+        background-color: rgba(61, 0, 160, 0.537);
+        padding: 0 !important;
+    }
     
     .blur{
         backdrop-filter: blur(10px);
